fa35398b49d8bea601f4ac8c314ff10247be715a
xxhomey19 <xxhomey19@gmail.com>
2017-09-20 23:46:33 +0800

    find new videos from collection videos

--- src/models/videos.js
+++ src/models/videos.js
@@ -1,11 +1,20 @@
+const subDays = require('date-fns/sub_days');
+const getTime = require('date-fns/get_time');
 const getDatabase = require('./database');
 
 const getNewVideos = async () => {
   const db = await getDatabase();
+  const now = Date.now();
+
   const newVideos = await db
-    .collection('new_videos')
-    .find()
-    .sort({ total_view_count: -1 })
+    .collection('videos')
+    .find({
+      updated_at: {
+        $gte: new Date(getTime(subDays(now, 1))),
+        $lte: new Date(now),
+      },
+    })
+    .sort({ score: -1 })
     .limit(3)
     .toArray();
   return newVideos;
